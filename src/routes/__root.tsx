12b5dd796d5465db778d7201c707b0b9709ac52f
Jack Watters <johnwatt@usc.edu>
2024-07-23 17:06:42 -0700

    Move footer back to root component.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,7 +4,6 @@ import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { Toaster } from "@/components/ui/sonner";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Nav from "@/components/nav";
-import Footer from "@/components/footer";
 
 export const Route = createRootRoute({
 	component: RootComponent,
@@ -26,5 +25,26 @@ function RootComponent() {
 			<TanStackRouterDevtools initialIsOpen={false} />
 			<ReactQueryDevtools initialIsOpen={false} />
 		</>
+	);
+}
+
+function Footer() {
+	return (
+		<footer className="h-12 border-t border-border w-full text-sm text-muted-foreground fixed bottom-0 z-10 bg-background">
+			<div className="mx-auto h-full flex items-center justify-between max-w-screen-lg px-8">
+				<div className="flex items-center gap-8">
+					{/* <Link className="hover:underline hover:opacity-90" to="/about">
+						About
+					</Link>
+					<Link className="hover:underline hover:opacity-90" to="/resources">
+						Resources
+					</Link> */}
+				</div>
+				<div className="flex items-center gap-4">
+					<a href="https://trollycare.com">&copy;TrollyCare</a>
+					<div>{new Date().getFullYear()}</div>
+				</div>
+			</div>
+		</footer>
 	);
 }
