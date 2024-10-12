445cfcd63f3c9bbe47d52e755bc7f2838f1026f0
Jack Watters <johnwatt@usc.edu>
2024-10-12 10:08:16 -0700

    Env variables.

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -9,3 +9,17 @@ export const CATEGORIES = [
 	"Caregiving Excellence: Support & Best Practices",
 	"Other",
 ] as const;
+
+export const PORT = import.meta.env.PORT || 8080;
+
+export const CLIENT_PORT = import.meta.env.CLIENT_PORT || 5173;
+
+export const IS_DEVELOPMENT = import.meta.env.NODE_ENV === "development";
+
+export const API_URL = IS_DEVELOPMENT
+	? `http://localhost:${PORT}`
+	: "automated-homecare-newsletter-production.up.railway.app";
+
+export const CLIENT_URL = IS_DEVELOPMENT
+	? `http://localhost:${CLIENT_PORT}`
+	: "https://trollycare-newsletter.vercel.app";
